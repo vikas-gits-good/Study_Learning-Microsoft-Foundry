74d1981c76494431b4f64187b20489077dd9c3a5
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-03-19 13:36:08 +0530

    got the chat to work and added azure key vault

---
+++ GettingStarted/OPAI_Chat/opai_chat.py
@@ -0,0 +1,36 @@
+import os
+
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from dotenv import load_dotenv
+from azure.keyvault.secrets import SecretClient
+
+load_dotenv()
+
+
+AZURE_KEY_VAULT_URI = os.getenv("AZURE_KEY_VAULT_URI")
+
+kvc = SecretClient(
+    vault_url=AZURE_KEY_VAULT_URI,
+    credential=DefaultAzureCredential(),
+)
+
+AZURE_SERVICE_ENDPOINT = kvc.get_secret("AZURE-SERVICE-ENDPOINT-EXPT-TEST").value
+AZURE_API_KEY = kvc.get_secret("AZURE-API-KEY-EXPT-TEST").value
+AZURE_OPAI_MODEL = "gpt-5.3-chat"
+
+
+ai_proj_clnt = AIProjectClient(
+    endpoint=AZURE_SERVICE_ENDPOINT,
+    credential=DefaultAzureCredential(),
+)
+
+opai_client = ai_proj_clnt.get_openai_client()
+
+rspn = opai_client.responses.create(
+    model=AZURE_OPAI_MODEL,
+    instructions="You are a helpful ai assistant",
+    input="Tell me about Microsoft Foundry",
+)
+
+print(rspn.output_text)
